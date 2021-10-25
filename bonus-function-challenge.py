5cbe3175b47741333623431967b0ba8b635bb4fd
Jinwoo Park <elvintern@gmail.com>
2021-10-26 11:32:05 +1300

    Update bonus-function-challenge.py

--- bonus-function-challenge.py
+++ bonus-function-challenge.py
@@ -1,39 +1,39 @@
-string_1 = "pineapple"
-print("string we messing with: ", string_1)
+# string_1 = "pineapple"
+# print("string we messing with: ", string_1)
 
-for letter in string_1:
-    print("letter in our string: ", letter)
-    if letter == "a":
-        print("ouch, this loop just hit an a")
-print("our string in uppercase: ", string_1.upper())
+# for letter in string_1:
+#     print("letter in our string: ", letter)
+#     if letter == "a":
+#         print("ouch, this loop just hit an a")
+# print("our string in uppercase: ", string_1.upper())
 
-string_2 = "kendrick lamar"
-print("string we messing with: ", string_2)
+# string_2 = "kendrick lamar"
+# print("string we messing with: ", string_2)
 
-for letter in string_2:
-    print("letter in our string: ", letter)
-    if letter == "a":
-        print("ouch, this loop just hit an a")
-print("our string in uppercase: ", string_2.upper())
+# for letter in string_2:
+#     print("letter in our string: ", letter)
+#     if letter == "a":
+#         print("ouch, this loop just hit an a")
+# print("our string in uppercase: ", string_2.upper())
 
 
-string_3 = "j cole"
-print("string we messing with: ", string_3)
+# string_3 = "j cole"
+# print("string we messing with: ", string_3)
 
-for letter in string_3:
-    print("letter in our string: ", letter)
-    if letter == "a":
-        print("ouch, this loop just hit an a")
-print("our string in uppercase: ", string_3.upper())
+# for letter in string_3:
+#     print("letter in our string: ", letter)
+#     if letter == "a":
+#         print("ouch, this loop just hit an a")
+# print("our string in uppercase: ", string_3.upper())
 
-string_4 = "first things first rest in peace uncle Phill"
-print("string we messing with: ", string_4)
+# string_4 = "first things first rest in peace uncle Phill"
+# print("string we messing with: ", string_4)
 
-for letter in string_4:
-    print("letter in our string: ", letter)
-    if letter == "a":
-        print("ouch, this loop just hit an a")
-print("our string in uppercase: ", string_4.upper())
+# for letter in string_4:
+#     print("letter in our string: ", letter)
+#     if letter == "a":
+#         print("ouch, this loop just hit an a")
+# print("our string in uppercase: ", string_4.upper())
 
 
 def find_a():
